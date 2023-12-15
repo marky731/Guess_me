e3a22dab50abf2a39005eb57159e6d620eaca144
mac <2200007052@stu.iku.edu.tr>
2023-12-15 09:35:05 +0300

    Initial Commit

---
+++ 2200007052_OOP_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2200007052_OOP_Project
+//
+//  Created by MAC on 15.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
